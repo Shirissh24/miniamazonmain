80f761ee43c9b68d0f352106d16ebdc4bd7f68dc
Shirissh24 <shirishsthapit24.com>
2025-02-07 11:36:30 +0545

    update

--- mini-amazon-main/UI/src/Home.jsx
+++ mini-amazon-main/UI/src/Home.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import ProductCard from "./components/productcard";
+import { Box } from "@mui/material";
 
 const Home = () => {
   return (
-    <div>
-      <ProductCard></ProductCard>
-    </div>
+    <Box
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "3rem",
+        margin: "3rem",
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+      }}
+    >
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </Box>
   );
 };
 
